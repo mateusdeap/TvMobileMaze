fff59ee02550d2a37beca32763e7c53656222c08
Mateus Pereira <mateusdeap@gmail.com>
2020-06-15 20:36:18 -0300

    Fixes toString override in Schedule.kt

--- app/src/main/java/com/example/tvmobilemaze/domain/Schedule.kt
+++ app/src/main/java/com/example/tvmobilemaze/domain/Schedule.kt
@@ -3,11 +3,21 @@ package com.example.tvmobilemaze.domain
 class Schedule(val time: String, val days: List<String>) {
     override fun toString(): String {
         var exhibitionDays = ""
-        for (day in days) {
-            exhibitionDays = if (day == days.last()) {
-                exhibitionDays.plus(" and $day")
-            } else {
-                exhibitionDays.plus(day)
+        if (days.size == 1) {
+            exhibitionDays = exhibitionDays.plus(days.first())
+        } else {
+            for (day in days) {
+                exhibitionDays = when (day) {
+                    days.last() -> {
+                        exhibitionDays.plus(" and $day")
+                    }
+                    days.first() -> {
+                        exhibitionDays.plus(day)
+                    }
+                    else -> {
+                        exhibitionDays.plus(", $day")
+                    }
+                }
             }
         }
 
